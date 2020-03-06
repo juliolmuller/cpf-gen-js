fbafa188c96331b91c0781cef1efa8f731861702
Julio Muller <juliodolago@gmail.com>
2020-03-06 00:07:08 -0300

    Created function to generate random numbers

--- src/core/index.js
+++ src/core/index.js
@@ -1,6 +1,20 @@
 const numOnly = require('@lacussoft/num-only')
 
 /**
+ * Generate a string of integer numbers
+ *
+ * @param {number} length Number of digits to be generated
+ * @return {string}
+ */
+const numGen = function(length) {
+  let string = ''
+  while (string.length < length) {
+    string += String(parseInt(Math.random() * 10))
+  }
+  return string
+}
+
+/**
  * Generate a valid CPF (Brazilian ID document) number sequence
  *
  * @param {object} [options]
