c04a253b2d37e116d38d3633d730cfcf0cee771e
Julio Muller <juliodolago@gmail.com>
2020-04-06 13:12:47 -0300

    Updated dependencies

--- dist/cpf-gen.js
+++ dist/cpf-gen.js
@@ -117,13 +117,19 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"GXLD":[function(require,module,exports) {
-var define;
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"Focm":[function(require,module,exports) {
-module.exports=function(e){return String(e).replace(/\D/g,"")};
-},{}]},{},["Focm"], null)
+})({"GX3L":[function(require,module,exports) {
+/**
+ * Removes any non-numeric character from a string chaing
+ *
+ * @param {string} target
+ * @returns {string} - Target string you want to clear from NaN characters
+ */
+module.exports = function(target) {
 
-},{}],"uKn2":[function(require,module,exports) {
+  return String(target).replace(/\D/g, '')
+}
+
+},{}],"odXC":[function(require,module,exports) {
 var define;
 // modules are defined as an array
 // [ module function, map of requires ]
@@ -462,7 +468,7 @@ var cpfGen = function cpfGen(options) {
 };
 
 module.exports = cpfGen;
-},{"@lacussoft/num-only":"GXLD","@lacussoft/cpf-fmt":"uKn2"}],"UeJd":[function(require,module,exports) {
+},{"@lacussoft/num-only":"GX3L","@lacussoft/cpf-fmt":"odXC"}],"UeJd":[function(require,module,exports) {
 window.cpfGen = require('./core');
 },{"./core":"rqMW"}]},{},["UeJd"], null)
 //# sourceMappingURL=/cpf-gen.js.map
